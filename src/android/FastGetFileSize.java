52b675f4431c866f81545f1ba65cc5b98cf4c59a
jery.M <1265085976@qq.com>
2018-06-20 12:01:21 +0800

    提交源代码

---
+++ src/android/FastGetFileSize.java
@@ -0,0 +1,60 @@
+package cordova.plugin.fastGetFileSize;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import android.os.Environment;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.File;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class FastGetFileSize extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+		if(action.equals("getFileSize")){
+			String externalRootDirectory = args.getString(0);
+			String filepath = args.getString(1);
+			this.getFileSize();
+		}
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+	
+	private void getFileSize(String externalRootDirectory,String filepath,CallbackContext callbackContext){
+		if(Environment.getExternalStorageState().equals(Environment.MEDIA_MOUNTED)){
+			try {
+				File file= new File(externalRootDirectory+"/"+filepath);
+				if (file.exists() && file.isFile()){ 
+                     JSONObject json = new JSONObject();
+                     json.put("fileSize",file.length());					 
+                     callbackContext.success(json);
+                }else{  
+                     callbackContext.error("file doesn't exist or is not a file");  
+                }  
+			} catch (Exception e) {
+				callbackContext.error(e.getMessage());
+			}
+		}
+		callbackContext.error("未发现外部存储");
+		
+	}
+}
---
+++ www/FastGetFileSize.js
@@ -0,0 +1,9 @@
+var exec = require('cordova/exec');
+
+exports.coolMethod = function (arg0, success, error) {
+    exec(success, error, 'FastGetFileSize', 'coolMethod', [arg0]);
+};
+
+exports.getFileSize = function (arg0, success, error) {
+    exec(success, error, 'FastGetFileSize', 'getFileSize', [arg0]);
+};
