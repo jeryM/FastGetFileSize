99e3add88712821b78b5dce09aaf36055d500f53
jery.M <1265085976@qq.com>
2018-06-20 20:14:46 +0800

    update

--- www/FastGetFileSize.js
+++ www/FastGetFileSize.js
@@ -4,6 +4,6 @@ exports.coolMethod = function (arg0, success, error) {
     exec(success, error, 'FastGetFileSize', 'coolMethod', [arg0]);
 };
 
-exports.getFileSize = function (arg0, success, error) {
-    exec(success, error, 'FastGetFileSize', 'getFileSize', [arg0]);
+exports.getFileSize = function (arg0,arg1, success, error) {
+    exec(success, error, 'FastGetFileSize', 'getFileSize', [arg0,arg1]);
 };
